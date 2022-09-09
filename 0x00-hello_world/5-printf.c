40710ece14439c663e6df26e515d6c7382b1b8b4
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-09 02:01:37 +0100

    mk ./5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - this is the description
+* prints with proper grammar, but the outcome is a piece of art
+* Return: int 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
