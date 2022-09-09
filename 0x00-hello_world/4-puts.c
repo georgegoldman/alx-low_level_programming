4bad81fe3e02db5a4ede29504a82a248aaf0fa32
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-09 01:21:38 +0100

    fixed betty doc style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 
 #include <stdio.h>
-
+/**
+ * main - print Programming is like building a multilingual puzzle
+ * and returns 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
