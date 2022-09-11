b9eed79602b6139682c2791285c3bfe95c37e348
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-11 15:59:42 -0700

    modified 6-size.c init long long int lli

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,11 @@
 */
 int main(void)
 {
+long long int lli;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
