fdced55858dde958231f2339b0ab825f74c1ee54
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-12 11:33:51 -0700

    rm what i needed aout

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: int 0
+*/
+int main(void)
+{
+	
+	int x = 0;
+	char y = 'a';
+	while (x < 10)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	while (y < 'g')
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
