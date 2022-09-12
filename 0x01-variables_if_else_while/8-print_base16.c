c902615e065503d1734a32618435f1f25a70bea7
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-12 11:42:04 -0700

    fixed the bug

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,20 +6,20 @@
 */
 int main(void)
 {
-	
-int x = 0;
-char y = 'a';
 
-while (x < 10)
-{
-putchar(x + '0');
-x++;
-}
-while (y < 'g')
-{
-putchar(y);
-y++;
-}
-putchar('\n');
-return (0);
+	int x = 0;
+	char y = 'a';
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	while (y < 'g')
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
 }
