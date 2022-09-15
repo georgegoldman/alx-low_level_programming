78f57629d5c484f9f11b4998409c4a53b897a1bd
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-15 13:14:48 +0100

    built the app

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+/**
+ * _islower - check latter case
+ * Return: int
+ */
+int _islower(int a)
+{
+	char b = 'a';
+	int i;
+	while(b <= 'z')
+	{
+		if ( a == b)
+		{
+			i = 1;
+		}
+        	b++;
+	}
+
+	return i;
+}
