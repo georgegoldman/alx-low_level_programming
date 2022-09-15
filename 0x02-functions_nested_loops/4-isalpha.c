349f4c5ec475b9e425ca33b6294dbee941d677c3
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-15 15:06:54 +0100

    built alphacheck console app

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+/**
+ * main - alphacheck console app
+ * @c: int argv
+ * Return: int
+ */
+int _isalpha(int c)
+{
+	int i = 0;
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	while (lowercase <= 'z' || uppercase <= 'Z')
+	{
+		if(lowercase == c || uppercase == c)
+		{
+			i = 1;
+		}
+		lowercase++;
+		uppercase++;
+	}
+	return (i);
+}
