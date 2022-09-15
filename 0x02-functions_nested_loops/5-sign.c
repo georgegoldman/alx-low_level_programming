44599bafa991b70b503dcc39eb9552bbf520bd08
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-15 15:54:16 +0100

    built the signed app

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - check signed numbers
+ * @n: int
+ * Return: signed int
+ */
+int print_sign(int n)
+{
+	signed int returnval = 0;
+	if (n > 0)
+	{
+		_putchar('+');
+		returnval = +1;
+	}
+	else if (n < 0)
+	{
+		_putchar('-')
+		returnval = -1;
+	}
+	else
+	{
+		_putchar('0');
+		returnval = 0;
+	}
+	return (returnval);
+}
