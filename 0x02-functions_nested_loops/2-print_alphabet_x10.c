1d1edef0e3ac20ef3aedfb6b4e6e12d29d126968
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-15 06:27:24 +0100

    added all

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet x10
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char a;
+	while (i < 10)
+	{
+		for (a = 'a', a <= 'z', a++)
+		{
+			_putchar(a);
+		}
+		i++;
+	}
+}
