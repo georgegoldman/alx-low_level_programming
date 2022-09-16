106fe6da627dca365649ee341372992c85afd355
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-16 16:42:58 +0100

    done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * void jack_bauer - minutes and hour counter
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int min = 0, hr = 0; hr_rm = 0, min_rm = 0;
+
+	while (hr <= 23)
+	{
+		while (min <= 59)
+		{
+			min_rm = min % 10;
+			hr_rm = hr % 10;
+			_putchar(hr/10 + '0');
+			_putchar(hr_rm + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min_rm + '0');
+			min++;
+			_putchar('\n');
+		}
+		hr++;
+		min = 0;
+	}
+}
