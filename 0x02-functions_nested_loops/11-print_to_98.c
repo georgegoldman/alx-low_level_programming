3a54a05f8e23b969fd06b834b6b3fdf35e40d00d
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-16 17:23:54 +0100

    fixed compile err

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,13 @@ void print_to_98(int x)
 {
 	if (x >= 98)
 	{
-		while (n > 98)
+		while (x > 98)
 			printf("%d, ", x--);
 		printf("%d\n", x);
 	}
 	else
 	{
-		while (n < 98)
+		while (x < 98)
 			printf("%d, ", x++);
 		printf("%d\n", x);
 	}
