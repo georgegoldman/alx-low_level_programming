ffd41eba2b48193570026b0e2824c2f62a12191d
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-21 14:26:24 +0100

    fixed out error

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,22 +5,17 @@
  *
  * str: Entry point parameter
  * @s: Entry point
- *
- * Return 0 Always.
+ * Return: 0 Always.
  */
-
-
 void print_rev(char *s)
 {
 	int index;
 
 	for (index = 0; s[index] != '\0'; index++)
-
+		;
 	for (index = index - 1; s[index] != '\0'; index--)
+	{
 		_putchar(s[index]);
-
+	}
 	_putchar('\n');
-
-
-
 }
