9330d4913a9c192523d34b9e0888161fe8ef845f
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-21 16:04:04 +0100

    fix betty issues

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
- * _atoi - a function that convert a string to an integer.
+/**
+ * _atoi - a function that convert a string to an integer
  * @s: char*
  * Return: int
  */
