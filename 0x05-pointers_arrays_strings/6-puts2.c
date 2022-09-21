7c61d6f3177a4867b0ffecb35a9edf89beb66174
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-21 14:50:32 +0100

    fixed betty error

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every other character of a string, starting with the first character, followed by a new line
+ * puts2 - a function prints char of a str, 1st char, new line
  * @str: char*
  * Return: void
  */
