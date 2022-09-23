e4c0bee4bc7cada1a148b757312b012564fd2fa5
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-23 19:04:46 +0100

    fixed error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _strncpy - function that copies a string
+ * _strncpy -  a function that copies a string
  * @dest: char
  * @src: char
  * @n: int
+ * Return: char
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+		dest[1] = src[i];
 	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
