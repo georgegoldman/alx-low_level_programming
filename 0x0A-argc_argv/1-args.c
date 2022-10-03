a92e87ef146575af514edd05c920243f5b989d46
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-10-03 11:17:21 +0100

    wrote a program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - a program that prints the number of arguments passed into it
+ * @argc: int
+ * @argv: char
+ * Return: int
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	_putchar('0' + argc);
+	return (0);
+}
